a2aef31cb6e840c9c78233a44ea10a4b69488b63
vladyc1234 <73032808+vladyc1234@users.noreply.github.com>
2026-07-15 17:23:09 +0300

    Initial Setup
    - Configure Java JDK in Environment Variables
    - Generate Spring-Maven project (using Spring Initializr)
    - Add Maven dependencies
    - Import MySQL docker image
    - Configure IDE

---
+++ game-store-api/src/main/java/com/gamestore/game_store_api/GameStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.gamestore.game_store_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreApiApplication.class, args);
+	}
+
+}
---
+++ game-store-api/src/test/java/com/gamestore/game_store_api/GameStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamestore.game_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
